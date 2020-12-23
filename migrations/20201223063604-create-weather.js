f1cbfc499d717407530894a33a44dc7f36861f99
Yuil Chae <romesway@gmail.com>
2020-12-23 20:34:31 +0900

    [server] setting fix2

--- migrations/20201223063604-create-weather.js
+++ migrations/20201223063604-create-weather.js
@@ -19,7 +19,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    })
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('weather');
