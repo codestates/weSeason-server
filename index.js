03e418a18ad913b9abe0f9a2caabbd12550921df
jeongho3786 <jeongho3786@gmail.com>
2020-12-25 00:00:06 +0900

    [server]arrange code

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const credentials = { key: privateKey, cert: certificate };
 const routerIdx = require('./router/index');
 const app = express()
 
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
