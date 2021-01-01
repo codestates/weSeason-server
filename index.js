4aba76d1b7159d8d60c1d6c26fe03a7749dee16c
jeongho3786 <jeongho3786@gmail.com>
2021-01-02 04:36:30 +0900

    [server]deploy setting, add dev setting

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ app.use(cors({
   origin: ["https://www.weseason4.com"],
   credentials: true,
 }));
+
 // app.use(cors());
+
+// app.use(cors({
+//   origin: ["https://localhost:3000"],
+//   credentials: true,
+// }));
 
 app.use(express.json());
 app.use(cookieParser());
